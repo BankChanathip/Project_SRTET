7f76edda4255fa4081c407ee0001ab4b753e728e
khampanpare88 <khampanpare88@gmail.com>
2022-04-18 09:22:53 +0700

    npm

--- server/index.js
+++ server/index.js
@@ -2,14 +2,22 @@
 
 const express = require("express");
 
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "SRTET",
+});
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/api", (req, res) => {
+app.get("/test", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
   
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
+
