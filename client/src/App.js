7df196ae7ef80a4769380d2b8a2663b80b343601
khampanpare88 <khampanpare88@gmail.com>
2022-04-25 13:21:34 +0700

    search

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Axios from 'axios'
 
 const Feed = () =>{
     const [data, setdata] = useState([]);
+    const [search, setSearch] = useState('');
 
     useEffect (() => {
       Axios.get("http://localhost:3001/").then((response) => {
@@ -11,17 +12,36 @@ const Feed = () =>{
       });
     }, []);
 
+    const searchPost = () => {
+      if(search != ''){
+          Axios.post("http://localhost:3001/", { 
+              item: search
+          }).then((response) => {
+              setdata(response.data);
+          })
+      }
+  };
+
   return (
-    <div className = "feed">
-      <h1>Data</h1>
-            <div>
-                {data.map((val)=> {
-                    return (
-                      <h5> {val.no}  {val.id}  {val.name}  {val.entry_reader} {val.entry_door} {val.entry_date_time} {val.exit_reader} {val.exit_door} {val.exit_date_time} </h5>
-                    ); 
-                })}
-            </div>
+    <div>
+      
+        <input type="text" placeholder="search item" name="item" onChange={(e)=>{setSearch(e.target.value)}} />
+        <button className="searchButt" onClick={searchPost}> search </button> 
+      
+
+      
+        <h1>Data</h1>
+          <div>
+            {data.map((val)=> {
+              return (                      
+                <h5> {val.no}  {val.id}  {val.name}  {val.entry_reader} {val.entry_door} {val.entry_date_time} {val.exit_reader} {val.exit_door} {val.exit_date_time} </h5> ); 
+            })}
+          </div>
+      
+
     </div>
+
+    
   );
 }
   
